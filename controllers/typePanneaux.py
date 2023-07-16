ef3fd653e4705801cc8d5cdf3988cb8d32ec9189
bayanga <antoine.bayanga@gmail.com>
2023-07-16 13:44:14 +0100

    updating models

---
+++ controllers/typePanneaux.py
@@ -0,0 +1,69 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+
+from models.typePanneau import TypePanneau
+from schemas.TypePanneaux import TypePanneauCreateSchema, TypePanneauSchema
+# a controller class to handle all actions on the TypePanneau model
+
+class TypePanneauController:
+    # get by id
+    @classmethod
+    def get(cls, db: Session, type_panneau_id: int) -> TypePanneauSchema:
+        # if type_panneau does not exist, throw an error
+        try:
+            type_panneau = TypePanneau.get(db, type_panneau_id)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_NOT_FOUND, detail=str(e))
+        if not type_panneau:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="TypePanneau not found")
+        return type_panneau
+    
+    # get all
+    @classmethod
+    def getAll(cls, db: Session) -> list[TypePanneauSchema]:
+        try:
+            return TypePanneau.getAll(db)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_NOT_FOUND, detail=str(e))
+        
+    # create
+    @classmethod
+    def create(cls, db: Session, type_panneau: TypePanneauCreateSchema) -> TypePanneauSchema:
+        try:
+            # check if CodeTypePanneau is unique
+            if db.query(TypePanneau).filter_by(CodeTypePanneau=type_panneau.CodeTypePanneau).first():
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="CodeTypePanneau already exists")
+            type_panneau = TypePanneau.create(db, type_panneau)
+            return type_panneau
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_NOT_FOUND, detail=str(e))
+        
+    # delete
+    @classmethod
+    def delete(cls, db: Session, type_panneau_id: int):
+        try:
+            # check if type_panneau exists
+            type_panneau = TypePanneau.get(db, type_panneau_id)
+            if not type_panneau:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="TypePanneau not found")
+            # delete type_panneau
+            type_panneau = TypePanneau.delete(db, type_panneau_id)
+            return type_panneau
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_NOT_FOUND, detail=str(e))
+    
+    # update
+    @classmethod
+    def update(cls, db: Session, type_panneau_id: int, libelleType: str) -> TypePanneauSchema:
+        try:
+            # check if type_panneau exists
+            type_panneau = TypePanneau.get(db, type_panneau_id)
+            if not type_panneau:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="TypePanneau not found")
+            # update type_panneau
+            type_panneau = TypePanneau.updateLibelleType(db, type_panneau_id, libelleType)
+            return type_panneau
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_NOT_FOUND, detail=str(e))
+    
--- models/typePanneau.py
+++ models/typePanneau.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, SmallInteger, ForeignKey
 from sqlalchemy.orm import relationship
 from db.Connexion import Base
+from sqlalchemy.orm import Session
 
 class TypePanneau(Base):
     __tablename__ = "TypePanneau"
@@ -16,3 +17,57 @@ class TypePanneau(Base):
 
     # Relation avec la table PanneauAffich
     panneaux = relationship("PanneauAffich", back_populates="type_panneau", lazy="joined", cascade="save-update, merge")
+
+    # get method
+    @classmethod
+    def get(cls, db: Session, type_panneau_id: int):
+        return db.query(cls).filter_by(IDTypePanneau=type_panneau_id).first()
+
+    # get all method
+    @classmethod
+    def getAll(cls, db: Session):
+        return db.query(cls).all()
+   
+    # get by codeTypePanneau
+    @classmethod
+    def getByCode(cls, db: Session, codeTypePanneau: str):
+        return db.query(cls).filter_by(CodeTypePanneau=codeTypePanneau).first()
+    
+    # create method
+    @classmethod
+    def create(cls, db: Session, type_panneau):
+        # Begin a transaction with the database
+        with db.begin():
+            # Add the 'type_panneau' object to the database session
+            db.add(type_panneau)
+            # Flush the session to persist the changes to the database
+            db.flush()
+        # Return the 'type_panneau' object
+        return type_panneau
+
+    # delete method
+    @classmethod
+    def delete(cls, db: Session, type_panneau_id: int):
+        # Create a query to retrieve all records in the table that have the given type_panneau_id
+        query = db.query(cls).filter_by(IDTypePanneau=type_panneau_id)
+        
+        # Execute the query and delete all the retrieved records
+        deleted_count = query.delete()
+        
+        # Commit the changes to the database
+        db.commit()
+
+        # Return True to indicate that the delete was successful
+        return True
+
+    # update method
+    @classmethod
+    def updateLibelleType(cls, db: Session, type_panneau_id: int, libelleType: str):
+        # Use a single query to update the LibelleType for all records with the given type_panneau_id
+        updated_count = db.query(cls).filter_by(IDTypePanneau=type_panneau_id).update({"LibelleType": libelleType})
+
+        # Commit the changes to the database
+        db.commit()
+
+        # Return the updated object
+        return cls.get(db, type_panneau_id)
