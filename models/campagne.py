a1763db865f965a7f7c51311af8bb4f85dba62a1
bayanga antoine <badinfo1987@gmail.com>
2023-08-04 08:56:10 +0100

    codes generation

--- models/campagne.py
+++ models/campagne.py
@@ -33,24 +33,41 @@ class CampagnePub(Base):
     # Relation avec la table Produit
     produits = relationship("CampagneProduit", lazy="joined", back_populates="campagne_pub")
 
+    # Generate code campagne
+    @classmethod
+    def generateCodeCampagne(cls, db: Session) -> str:
+        # get the current year's last 2 digits
+        year = str(datetime.now().year)[-2:]
+        # Query the database to get the last generated CodeCampagne
+        lastGeneratedCodeCampagne: CampagnePub = db.query(cls).order_by(cls.IDCampagnePub.desc()).first()
+        # extract the last 4 digits from the last generated CodeCampagne
+        if lastGeneratedCodeCampagne:
+            lastGeneratedCodeCampagne = lastGeneratedCodeCampagne.CodeCampagne[-4:]
+            lastGeneratedCodeCampagne = str(int(lastGeneratedCodeCampagne) + 1).zfill(4)
+        else:
+            lastGeneratedCodeCampagne = "0001"
+        return "CAP" + year + lastGeneratedCodeCampagne
+
     # get by ID
     @classmethod
-    def get(cls, db: Session, IDCampagnePub: int):
+    def get(cls, db: Session, IDCampagnePub: int) -> 'CampagnePub':
         return db.query(cls).filter(cls.IDCampagnePub == IDCampagnePub).first()
     
     # get by CodeCampagne
     @classmethod
-    def get_by_code(cls, db: Session, CodeCampagne: str):
+    def get_by_code(cls, db: Session, CodeCampagne: str) -> 'CampagnePub':
         return db.query(cls).filter(cls.CodeCampagne == CodeCampagne).first()
     
     # get all
     @classmethod
-    def get_all(cls, db: Session):
+    def get_all(cls, db: Session) -> list['CampagnePub']:
         return db.query(cls).all()
     
     # create
     @classmethod
-    def create(cls, db: Session, campagne):
+    def create(cls, db: Session, campagne: 'CampagnePub') -> 'CampagnePub':
+        # Generate the CodeCampagne automatically
+        campagne.CodeCampagne = cls.generateCodeCampagne(db)
         # set CreatedAt and UpdatedAt
         campagne.CreatedAt = campagne.UpdatedAt = datetime.now().isoformat()
         db.add(campagne)
@@ -60,7 +77,7 @@ class CampagnePub(Base):
     
     # update
     @classmethod
-    def update(cls, db:Session, campagne):
+    def update(cls, db:Session, campagne: 'CampagnePub') -> 'CampagnePub':
         # set UpdatedAt
         campagne.UpdatedAt = datetime.now().isoformat()
         db.add(campagne)
@@ -70,7 +87,7 @@ class CampagnePub(Base):
     
     # delete
     @classmethod
-    def delete(cls, db:Session, IDCampagnePub):
+    def delete(cls, db:Session, IDCampagnePub: int) -> bool:
         # get campagne_pub
         campagne_pub = cls.get(db, IDCampagnePub)
         if campagne_pub:
