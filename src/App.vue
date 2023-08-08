95bce8227a6b6f967e308b1378c39e784188f6da
bayanga antoine <badinfo1987@gmail.com>
2023-08-08 18:12:20 +0100

    registration

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <!-- Render the navbar component -->
-    <router-view name="navbar"></router-view>
-    
-    <!-- Render the sidebar component -->
-    <router-view name="sidebar"></router-view>
-
+  <!-- Render the navbar component -->
+  <router-view name="navbar"></router-view>
+  
+  <!-- Render the sidebar component -->
+  <router-view name="sidebar"></router-view>
+  <div class="rounded-lg dark:border-gray-700 mt-14">
     <!-- Render the default component -->
-    <router-view></router-view>
+    <router-view class="min-h-screen"></router-view>
   </div>
 </template>
--- src/views/RegistrationVue.vue
+++ src/views/RegistrationVue.vue
@@ -132,8 +132,8 @@ const register = async (event) => {
 <template>
     <Notification v-if="show_notification" :type="notification_type" :message="notification_message"
         :duration="notification_duration" />
-    <div class="flex items-center justify-center dark">
-        <div class="rounded-sm bg-white shadow-default dark:bg-boxdark w-2/4">
+    <div class="flex items-center justify-center min-h-screen  dark:bg-gray-800 dark:border-gray-700">
+        <div class="rounded-sm bg-white shadow-default  dark:bg-gray-800 dark:border-gray-700 w-2/4">
             <div class="w-full p-4 sm:p-12.5 xl:p-17.5">
 
                 <h2
@@ -147,125 +147,64 @@ const register = async (event) => {
                 </div>
 
                 <form>
-                    <div class="mb-4">
-                        <label class="mb-2.5 block font-medium text-black dark:text-white">username</label>
-                        <div class="relative">
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <div>
+                            <label for="username" class="mb-2.5 block font-medium text-black dark:text-white">username</label>
                             <input type="text" placeholder="Enter your username" v-model="username"
-                                class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
-
-                            <span class="absolute right-4 top-4">
-                                <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <g opacity="0.5">
-                                        <path
-                                            d="M11.0008 9.52185C13.5445 9.52185 15.607 7.5281 15.607 5.0531C15.607 2.5781 13.5445 0.584351 11.0008 0.584351C8.45703 0.584351 6.39453 2.5781 6.39453 5.0531C6.39453 7.5281 8.45703 9.52185 11.0008 9.52185ZM11.0008 2.1656C12.6852 2.1656 14.0602 3.47185 14.0602 5.08748C14.0602 6.7031 12.6852 8.00935 11.0008 8.00935C9.31641 8.00935 7.94141 6.7031 7.94141 5.08748C7.94141 3.47185 9.31641 2.1656 11.0008 2.1656Z"
-                                            fill="" />
-                                        <path
-                                            d="M13.2352 11.0687H8.76641C5.08828 11.0687 2.09766 14.0937 2.09766 17.7719V20.625C2.09766 21.0375 2.44141 21.4156 2.88828 21.4156C3.33516 21.4156 3.67891 21.0719 3.67891 20.625V17.7719C3.67891 14.9531 5.98203 12.6156 8.83516 12.6156H13.2695C16.0883 12.6156 18.4258 14.9187 18.4258 17.7719V20.625C18.4258 21.0375 18.7695 21.4156 19.2164 21.4156C19.6633 21.4156 20.007 21.0719 20.007 20.625V17.7719C19.9039 14.0937 16.9133 11.0687 13.2352 11.0687Z"
-                                            fill="" />
-                                    </g>
-                                </svg>
-                            </span>
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="username" required/>
                         </div>
-                    </div>
 
-                    <div class="mb-4">
-                        <div class="mb-4">
-                            <label class="mb-2.5 block font-medium text-black dark:text-white">Email</label>
-                            <div class="relative">
-                                <input type="email" placeholder="Enter your email" v-model="email"
-                                    :class="[emailError ? 'border-red-500' : '', 'w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary']" />
-
-                                <span class="absolute right-4 top-4">
-                                    <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <g opacity="0.5">
-                                            <path
-                                                d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-                                                fill="" />
-                                        </g>
-                                    </svg>
-                                </span>
-                            </div>
+                        <div>
+                            <label for="email" class="mb-2.5 block font-medium text-black dark:text-white">Email</label>
+                            <input type="email" id="email" placeholder="Enter your email" v-model="email"
+                                :class="[emailError ? 'border-red-500' : '', 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']" />
                         </div>
-                    </div>
 
-                    <div class="mb-4">
-                        <label class="mb-2.5 block font-medium text-black dark:text-white">Password</label>
-                        <div class="relative">
-                            <input type="password" placeholder="Enter your password" v-model="password"
-                                :class="[passwordError ? 'border-red-500' : '', 'w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary']" />
-
-                            <span class="absolute right-4 top-4">
-                                <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <g opacity="0.5">
-                                        <path
-                                            d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
-                                            fill="" />
-                                        <path
-                                            d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
-                                            fill="" />
-                                    </g>
-                                </svg>
-                            </span>
+                        <div>
+                            <label for="password" class="mb-2.5 block font-medium text-black dark:text-white">Password</label>
+                            <input type="password" id="password" placeholder="Enter your password" v-model="password"
+                                :class="[passwordError ? 'border-red-500' : '', 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']" />
                         </div>
-                    </div>
 
-                    <div class="mb-6">
-                        <label class="mb-2.5 block font-medium text-black dark:text-white">Re-type Password</label>
-                        <div class="relative">
+                        <div>
+                            <label class="mb-2.5 block font-medium text-black dark:text-white">Re-type Password</label>
                             <input type="password" placeholder="Enter your password" v-model="repeatPassword"
-                                :class="[repeatPasswordError ? 'border-red-500' : '', 'w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary']" />
-
-                            <span class="absolute right-4 top-4">
-                                <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <g opacity="0.5">
-                                        <path
-                                            d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
-                                            fill="" />
-                                        <path
-                                            d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
-                                            fill="" />
-                                    </g>
-                                </svg>
-                            </span>
+                                :class="[repeatPasswordError ? 'border-red-500' : '', 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']" />
                         </div>
-                    </div>
 
-                    <div class="mb-4">
-                        <label class="mb-2.5 block font-medium text-black dark:text-white">User Role</label>
-                        <div class="relative">
+                        <div>
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User Role</label>
                             <select v-model="roles"
-                                class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option v-for="role in userRoles" :value="role" :key="role">{{ role }}</option>
                             </select>
+                            <!-- <div class="relative">
+                        </div> -->
                         </div>
-                    </div>
 
-                    <div class="mb-4">
-                        <label class="mb-2.5 block font-medium text-black dark:text-white">User Status</label>
-                        <div class="relative">
-                            <select v-model="status"
-                                class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
+                        <div>
+
+                            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User
+                                Status</label>
+                            <select v-model="status" id="status"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option v-for="status in userStatuses" :value="status" :key="status">{{ status }}</option>
                             </select>
+
+                        </div>
+
+                        <button @click="register"
+                            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                            Sign Up
+                        </button>
+
+                        <div class="mt-6 text-center">
+                            <p class="font-medium text-sm text-gray-900 dark:text-white">
+                                Already have an account?
+                                <RouterLink to="/login" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign in</RouterLink>
+                            </p>
                         </div>
                     </div>
-
-                    <button @click="register"
-                        class="w-full cursor-pointer rounded-lg border border-primary bg-red-500 p-4 font-medium text-white transition hover:bg-opacity-90">
-                        Sign Up
-                    </button>
-
-                    <div class="mt-6 text-center">
-                        <p class="font-medium">
-                            Already have an account?
-                            <RouterLink to="/login" class="text-primary">Sign in</RouterLink>
-                        </p>
-                    </div>
-
                 </form>
             </div>
         </div>
